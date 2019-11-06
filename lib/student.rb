2be8d3bbb59388de9bc0390f3109fc1f50fda4a5
stef-codes <stefcodes@gmail.com>
2019-11-06 10:06:42 -0800

    add methods

--- lib/student.rb
+++ lib/student.rb
@@ -14,13 +14,21 @@ class Student
     # remember each row should be a new instance of the Student class
     sql = <<-SQL
       SELECT * 
-      FROM songs
+      FROM students
     SQL
+    DB[:conn].execute(sql)
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE name = ?
+      LIMIT 1
+    SQL 
+    DB[:conn].e
   end
   
   def save
