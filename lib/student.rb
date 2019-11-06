22e4631b4d8b7d0ffd0c23f6a4fb18930f591187
stef-codes <stefcodes@gmail.com>
2019-11-06 10:24:32 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -16,6 +16,7 @@ class Student
       SELECT * 
       FROM students
     SQL
+
     DB[:conn].execute(sql)
   end
 
@@ -28,7 +29,10 @@ class Student
       WHERE name = ?
       LIMIT 1
     SQL 
-    DB[:conn].e
+
+    DB[:conn].execute(sql, name).map do |row| 
+      self.new_from_db(row) 
+    end.first 
   end
   
   def save
